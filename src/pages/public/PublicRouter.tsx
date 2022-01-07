3176a0722d6bbc07ccd7c6009892ebf2674af03e
whoAmI-0000 <devKica777@gmail.com>
2022-01-07 14:23:14 +0100

    update readme

--- src/pages/public/PublicRouter.tsx
+++ src/pages/public/PublicRouter.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router";
 import EasterEgg from "./EasterEgg";
 import WelcomePage from "./WelcomePage";
+import LoginPage from "./LoginPage";
 
 const PublicMainPage = () => {
   return (
     <Routes>
       <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/easterEgg" element={<EasterEgg />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
